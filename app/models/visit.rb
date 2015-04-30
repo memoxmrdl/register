b2ca5848778e8ebc69e1461baed721bd2760f90d
memoxmrdl <jmemox@gmail.com>
2015-04-30 12:26:59 -0500

    Pruebas unitarias del modelo Visit

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,4 +1,6 @@
 class Visit < ActiveRecord::Base
   belongs_to :logbook
   has_one :office
+
+  validates :register_at, presence: true
 end
--- spec/models/visit_spec.rb
+++ spec/models/visit_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Visit, type: :model do
-  
+  it 'un registro invalido' do
+    visit = Visit.new
+
+    visit.should_not be_valid
+  end
+
+  it 'un registro valido' do
+    visit = Visit.new
+    visit.register_at = DateTime.now
+
+    visit.should be_valid
+  end
 end
