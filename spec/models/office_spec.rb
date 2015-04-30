83df0c945457e6a1d91f870ae56d72a77b67a940
memoxmrdl <jmemox@gmail.com>
2015-04-30 12:00:46 -0500

    Cambiar a tipo de oficina para identificar pruebas

--- spec/models/office_spec.rb
+++ spec/models/office_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Office, type: :model do
-  it 'Un registro invalido' do
+  it 'Un registro oficina invalido' do
     office = Office.new
 
     office.should_not be_valid
   end
 
-  it 'Un registro valido' do
+  it 'Un registro oficina valido' do
     office = Office.new
     office.name = 'Modulo de computadoras'
 
     office.should be_valid
   end
 
-  it 'Cuando un registro ya existe' do
+  it 'Cuando un registro oficina ya existe' do
     create(:office)
 
     office = Office.new
