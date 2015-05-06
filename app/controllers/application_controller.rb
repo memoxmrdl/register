b1242c5ac81b13c0e028e0a8a375d58cebb83f0c
memoxmrdl <jmemox@gmail.com>
2015-05-06 13:59:35 -0500

    Evaluar vista de acuerdo por controllador

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  layout :layout_by_resource
+
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || stored_location_for(resource) || logbooks_path
+  end
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
   end
 end
