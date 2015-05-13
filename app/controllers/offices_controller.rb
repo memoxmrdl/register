aca528303e7fc47e017cbe2625da6ad0b5c5eee0
memoxmrdl <jmemox@gmail.com>
2015-05-13 09:59:05 -0500

    Flow de departamentos

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -11,8 +11,36 @@ class OfficesController < ApplicationController
     if @office.save
       redirect_to offices_path, notice: 'Departamento creado'
     else
-      flash.now[:alert] = 'Departamento ya existe o es nulo'
+      flash.now[:alert] = 'Departamento ya existe o es vacio'
       render :index
+    end
+  end
+
+  def edit
+    @offices = Office.all
+    @office = Office.find(params[:id])
+  end
+
+  def update
+    @office = Office.find(params[:id])
+
+    if @office
+      @office.update_attributes(office_params)
+      redirect_to offices_path, notice: 'Departamento actualizado'
+    else
+      flash.now[:alert] = 'Departamento ya existe o es vacio'
+      render :edit
+    end
+  end
+
+  def destroy
+    @office = Office.find(params[:id])
+
+    if @office
+      @office.destroy
+      redirect_to offices_path, notice: 'Departamento eliminado'
+    else
+      redirect_to offices_path, alert: 'Departamento no existe para eliminar'
     end
   end
 
