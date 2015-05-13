ee4ff52829e44c1fa4de1f734f29a651a448101c
memoxmrdl <jmemox@gmail.com>
2015-05-13 09:21:21 -0500

    Trabajo en progreso de departamentos

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,4 +1,24 @@
 class OfficesController < ApplicationController
   def index
+    @offices = Office.all
+    @office = Office.new
+  end
+
+  def create
+    @offices = Office.all
+    @office = Office.new(office_params)
+
+    if @office.save
+      redirect_to offices_path, notice: 'Departamento creado'
+    else
+      flash.now[:alert] = 'Departamento ya existe o es nulo'
+      render :index
+    end
+  end
+
+  private
+
+  def office_params
+    params.require(:office).permit(:name)
   end
 end
