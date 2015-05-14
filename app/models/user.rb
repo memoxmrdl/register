6aed57edd51e0fdf4e7b1f9796cf83159dacf0e8
memoxmrdl <jmemox@gmail.com>
2015-05-14 11:11:03 -0500

    Agregar un campo nombre a tabla de users y mostrar en el dropmenu

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :name, presence: true
+
   def admin?
     admin
   end
