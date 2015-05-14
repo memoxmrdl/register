2547edb7fdb216fb3901df28ff3209a096cd2139
memoxmrdl <jmemox@gmail.com>
2015-05-14 10:30:24 -0500

    Ocultar los alert automaticamente

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,3 +45,11 @@ function getBase64Image(imgElem) {
     return dataURL;
   }
 }
+
+onLoad(function(){
+  window.setTimeout(function() {
+    $('.alert').fadeTo(500, 0).slideUp(500, function(){
+      $(this).remove();
+    });
+  }, 1000);
+});
