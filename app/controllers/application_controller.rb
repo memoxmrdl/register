02135e6f8fd2de5c813379d984a27d0b411c0599
memoxmrdl <jmemox@gmail.com>
2015-06-03 13:41:27 -0500

    Es texto no interpolacion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,9 @@ class ApplicationController < ActionController::Base
 
   def layout_by_resource
     if devise_controller?
-      "devise"
+      'devise'
     else
-      "application"
+      'application'
     end
   end
 end
