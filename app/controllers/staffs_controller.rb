fed555e626d8ac8b712f2ba4c04cd2497d1b3a73
memoxmrdl <jmemox@gmail.com>
2015-06-03 23:21:07 -0500

    CRUD funcional de staff

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -3,4 +3,51 @@ class StaffsController < ApplicationController
     @office = Office.friendly.find(params[:office_id])
     @staff = Staff.new
   end
+
+  def create
+    @office = Office.friendly.find(params[:office_id])
+    @staff = Staff.new(staff_params)
+    @staff.office = @office
+
+    if @staff.save
+      redirect_to office_path(@office), notice: 'Staff creado'
+    else
+      flash.now[:error] = 'Staff no creado'
+      render :new
+    end
+  end
+
+  def edit
+    @staff = Staff.find(params[:id])
+    @office = @staff.office
+  end
+
+  def update
+    @staff = Staff.find(params[:id])
+
+    if @staff.update_attributes(staff_params)
+      redirect_to office_path(@staff.office), notice: 'Staff actualizado'
+    else
+      flash.now[:error] = 'Staff ya existe o campo vacio'
+      render :edit
+    end
+  end
+
+  def destroy
+    @staff = Staff.find(params[:id])
+    @office = @staff.office
+
+    if @staff
+      @staff.destroy
+      redirect_to office_path(@office), notice: 'Staff eliminado'
+    else
+      redirect_to office_path(@office), alert: 'Staff no existe para eliminar'
+    end
+  end
+
+  private
+
+  def staff_params
+    params.require(:staff).permit(:name)
+  end
 end
--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,4 +1,6 @@
 class Staff < ActiveRecord::Base
+  default_scope { order('created_at DESC') }
+
   has_one :position
   belongs_to :office
 end
