4038518b75b2304180bccd97628ef3e0072fcd84
memoxmrdl <jmemox@gmail.com>
2015-06-08 17:58:45 -0500

    Agregar un select_tag para identificar a que personal va a visitar

--- app/helpers/visits_helper.rb
+++ app/helpers/visits_helper.rb
@@ -1,31 +1,27 @@
 module VisitsHelper
-  def select_logbooks
-    select_tag(
-      'select_logbook',
-      options_for_select(@logbooks.map { |logbook| [logbook.full_name, logbook.id,
-        { 'data-photo' => image_url(logbook.photo.url), 'data-crendential' => image_url(logbook.credential.url) }]
-      }),
-      include_blank: true,
-      title: 'Seleccionar visitante',
-      data: {
-        'live-search' => 'true',
-        'size' => '10',
-        'width' => '100%'
-      }
-    )
-  end
-
   def select_offices
     select_tag(
       'select_office',
       options_from_collection_for_select(@offices, 'id', 'name'),
       include_blank: true,
-      title: '',
+      title: 'Departamento',
       data: {
         'size' => '10',
         'width' => '100%'
       },
-      class: 'selectpicker'
+      class: 'selectpicker '
+    )
+  end
+
+  def select_staff
+    select_tag('select_staff', '',
+      include_blank: true,
+      title: 'Staff',
+      data: {
+        'size' => '10',
+        'width' => '100%'
+      },
+      class: 'selectpicker '
     )
   end
 end
