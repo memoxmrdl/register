0eb3fad125556ca4cf75d1cec42aa7a074fae305
memoxmrdl <jmemox@gmail.com>
2015-06-08 18:04:07 -0500

    Remover no es necesario

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -6,8 +6,8 @@ class Visit < ActiveRecord::Base
   validates_uniqueness_of :logbook_id, scope: :office_id, conditions: -> { where(output_at: nil) }
 
   scope :current_visits,
-    -> { Visit.where('register_at::date = ? AND output_at IS NULL', DateTime.now.to_date) }
+    -> { where('register_at::date = ? AND output_at IS NULL', DateTime.now.to_date) }
 
   scope :past_visits,
-    -> { Visit.where('register_at::date = ? AND output_at IS NOT NULL', DateTime.now.to_date) }
+    -> { where('register_at::date = ? AND output_at IS NOT NULL', DateTime.now.to_date) }
 end
