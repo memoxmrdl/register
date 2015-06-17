7d4a7f05ce071e79204e2eba64604868a8d04c74
memoxmrdl <jmemox@gmail.com>
2015-06-16 23:26:09 -0500

    Quitar metodo

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,13 +1,5 @@
 class Staff < ActiveRecord::Base
   default_scope { order('created_at DESC') }
-
-  belongs_to :office
-
   has_one :visit
-
-  class << self
-    def more_visited
-      find(Visit.maximum(:staff_id))
-    end
-  end
+  belongs_to :office
 end
