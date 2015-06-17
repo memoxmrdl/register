46f43985badafedfcd8abf0da3e59e5f2c4c744c
memoxmrdl <jmemox@gmail.com>
2015-06-16 23:25:22 -0500

    Quitar metodo self

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,16 +1,9 @@
 class Office < ActiveRecord::Base
   extend FriendlyId
-
   friendly_id :name, use: :slugged
 
   has_many :visit
   has_many :staff
 
   validates :name, presence: true, uniqueness: true
-
-  class << self
-    def more_visited
-      find(Visit.maximum(:office_id))
-    end
-  end
 end
